6a155032f3d7f959eab6a67d9040832e15c372ab
pedro-ernesto <silveira-pedro@outlook.com>
2022-02-21 02:41:39 -0300

    lista e counter adicionados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,12 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState=>[...oldState,data]);
   }
 
   function handleToggleTaskDone(id: number) {
